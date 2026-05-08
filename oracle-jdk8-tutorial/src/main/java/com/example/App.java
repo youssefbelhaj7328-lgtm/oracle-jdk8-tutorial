6a8007970e029fe496313c9c025297100ae3fbbb
Youssef <youssefbelhaj7328@gmail.com>
2026-05-08 18:47:03 +0200

    antes de cambiarme a ver otro commit

--- oracle-jdk8-tutorial/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial/src/main/java/com/example/App.java
@@ -1,51 +1,10 @@
 package com.example;
 
-/*
-* ¿Que es un metodo? 
-* 
-* Es un fragmento o bloque de codigo, de sentencias java en este caso, encerrado
-* entre llaves de apertura y cierre {}, que se declara una sola vez en el codigo del programa
-* o que tambien podria existir en la biblioteca o API de Java, y se puede invocar o llamar
-* desde muchas partes del programa, pasandole o no parametros entre parentesis, despues del 
-* nombre del fragmento de codigo.
-* 
-* Cuando el fragmento de codigo esta fuera de una clase (LO CUAL NO ES POSIBLE EN JAVA),
-* se le llama subrutina o funcion, pero cuando esta dentro de una clase se le llama METODO
-* 
-* La ventaja que tiene utilizar metodos es que cuando hay que modificarlos solamente hay 
-* que hacerlo en un solo sitio.
-* 
-* Y nuestro codigo de Java se inicia en un metodo que busca la JVM (Java Virtual Machine) 
-* llamado main(), que podrian existir varios metodos main en nuestro proyecto, pero seria 
-* una mala practica de programacion, porque habria que estarle indicando a la JVM por
-* cual metodo main queremos arrancar nuestro programa
-* Supongamos que en distintas partes del proyecto tenemos 
-* que sumar dos operandos, es decir, dos datos que se almacenan
-* en variables */
-
 public class App {
 
-	static double sumar(double a, double b) {
-
-		double c = a + b;
-
-		return c;
-	}
-
 	public static void main(String[] args) {
-		double a = 2.2;
-		double b = 2.2;
-
-		sumar(a, b);
-
-		a = 3;
-		b = 3;
-
-		sumar(a, b);
-
-		a = 4;
-		b = 4;
-
-		sumar(a / 2, b * 2);
+/**
+ * ¿como crear o instanciar objetos?
+ */
 	}
 }
